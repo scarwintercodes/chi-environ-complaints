0b62aaacdaf23283ca5b103856f900cc7796cc09
Scar <49616427+scarwintercodes@users.noreply.github.com>
2025-06-01 18:26:57 -0500

    upload files

--- script.R
+++ script.R
@@ -4,10 +4,21 @@ library(jsonlite)
 library(httr)
 library(tidyverse)
 
+#API call from Visual Crossing
 res <-GET("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Chicago?unitGroup=us&key=AMUYTVER39XM8FVY84ZLQSDHS&contentType=json")
 res
 
+#extract JSON format and preview
 rawToChar(res$content)
 
 data <- fromJSON(rawToChar(res$content))
 names(data)
+
+######################################
+
+## load CDPH data
+cdph <- GET("https://data.cityofchicago.org/resource/fypr-ksnz.json")
+cdph
+
+rawToChar(cdph$content)
+cdph_data <- fromJSON(rawToChar(cdph$content))
