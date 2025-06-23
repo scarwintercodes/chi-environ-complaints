31f08ced1920dc429953a273691d668ca5912724
Scar <49616427+scarwintercodes@users.noreply.github.com>
2025-06-23 16:02:24 -0500

    Add inspection steps for CDPH geoJSON data
    Added code to inspect the structure and CRS of the loaded CDPH geoJSON data, and to separate and investigate geometry and attribute components for further analysis.

--- script.R
+++ script.R
@@ -17,6 +17,8 @@ names(data)
 ## load CDPH data as geoJSON file
 cdph <-  sf::st_read("https://data.cityofchicago.org/resource/fypr-ksnz.geojson?$limit=50000")
 
+str(cdph)
+
 # find and remove missing lat/longs
 sum(is.na(cdph$latitude))
 sum(is.na(cdph$longitude))
@@ -24,4 +26,15 @@ sum(is.na(cdph$longitude))
 
 cdph_clean <- cdph %>% 
   filter(!is.na(latitude), !is.na(longitude))
+
+########## INSPECTION STEPS #################
+#print CRS, WGS 84
+print(sf::st_crs(cdph_clean))
+
+cdph_attributes_only <- sf::st_drop_geometry(cdph_clean)
+cdph_geo_only <- sf::st_geometry(cdph_clean)
+
+# Investigate geo object
+glimpse(cdph_geo_only)
+class(cdph_geo_only)
 
